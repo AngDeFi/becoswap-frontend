62bfdbba283cd26408622616f42fa0b5f429d5c7
AngDeFi <84949712+AngDeFi@users.noreply.github.com>
2021-06-15 11:09:23 +0700

    Update meta.ts

--- src/config/constants/meta.ts
+++ src/config/constants/meta.ts
@@ -1,44 +1,44 @@
 import { PageMeta } from './types'
 
 export const DEFAULT_META: PageMeta = {
-  title: 'BecoSwap',
+  title: 'PoliEx Finance',
   description:
-    'The most popular AMM on BSC by user count! Earn BECO through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by BecoSwap), NFTs, and more, on a platform you can trust.',
+    'The most popular AMM on BSC by user count! Earn POLEX through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by PoliEx Finance), NFTs, and more, on a platform you can trust.',
   image: 'https://becoswap.com/images/hero.jpg',
 }
 
 export const customMeta: { [key: string]: PageMeta } = {
   '/': {
-    title: 'Home | BecoSwap',
+    title: 'Home | PoliEx Finance',
   },
   '/competition': {
-    title: 'Trading Battle | BecoSwap',
+    title: 'Trading Battle | PoliEx Finance',
   },
   '/prediction': {
-    title: 'Prediction | BecoSwap',
+    title: 'Prediction | PoliEx Finance',
   },
   '/farms': {
-    title: 'Farms | BecoSwap',
+    title: 'Farms | PoliEx Finance',
   },
   '/pools': {
-    title: 'Pools | BecoSwap',
+    title: 'Pools | PoliEx Finance',
   },
   '/lottery': {
-    title: 'Lottery | BecoSwap',
+    title: 'Lottery | PoliEx Finance',
   },
   '/collectibles': {
-    title: 'Collectibles | BecoSwap',
+    title: 'Collectibles | PoliEx Finance',
   },
   '/ifo': {
-    title: 'Initial Farm Offering | BecoSwap',
+    title: 'Initial Farm Offering | PoliEx Finance',
   },
   '/teams': {
-    title: 'Leaderboard | BecoSwap',
+    title: 'Leaderboard | PoliEx Finance',
   },
   '/profile/tasks': {
-    title: 'Task Center | BecoSwap',
+    title: 'Task Center | PoliEx Finance',
   },
   '/profile': {
-    title: 'Your Profile | BecoSwap',
+    title: 'Your Profile | PoliEx Finance',
   },
 }
