229379e5f44530447c0efbf2789477f110e1d193
AngDeFi <84949712+AngDeFi@users.noreply.github.com>
2021-06-15 11:11:18 +0700

    Update pools.ts

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -15,6 +15,20 @@ const pools: PoolConfig[] = [
     tokenPerBlock: '10',
     sortOrder: 1,
     isFinished: false,
+  },
+  {
+    sousId: 1,
+    stakingToken: tokens.wbnb,
+    earningToken: tokens.polex,
+    contractAddress: {
+       97: '',
+       56: '0x9a9d8Ec7bF0DAb04DB5AdE3305bd784471f9b102',
+    },
+    poolCategory: PoolCategory.CORE,
+    harvest: true,
+    tokenPerBlock: '5',
+    sortOrder: 1,
+    isFinished: false,
   }
 ]
 
